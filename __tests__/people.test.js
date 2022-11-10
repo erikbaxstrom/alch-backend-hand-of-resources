8696bf775b672de74baac6d67d46aafef576a564
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-10 10:22:01 -0800

    add test for /people route

---
+++ __tests__/people.test.js
@@ -0,0 +1,26 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+// const { request } = require('express');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('Test People Routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('/people should return a list of people and their ids', async () => {
+    const resp = await request(app).get('/people');
+    expect(resp.status).toBe(200);
+    const expectedResponse = [
+      { id: '1', firstName: 'Rod', lastName: 'Huntar' },
+      { id: '2', firstName: 'Cole', lastName: 'Benediktsson' },
+      { id: '3', firstName: 'Darb', lastName: 'Hubbert' },
+      { id: '4', firstName: 'Rafe', lastName: 'Killelea' },
+      { id: '5', firstName: 'Jim', lastName: 'Hibling' },
+    ];
+    expect(resp.body).toEqual(expectedResponse);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+});
