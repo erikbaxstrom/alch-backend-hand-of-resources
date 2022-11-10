7d80e4b62aaa189072dbdde2989c59c12c32d2f1
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-10 09:53:13 -0800

    add #1 db setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,15 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS people;
+
+create table people (
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50)
+);
+insert into people (first_name, last_name) values ('Rod', 'Huntar');
+insert into people (first_name, last_name) values ('Cole', 'Benediktsson');
+insert into people (first_name, last_name) values ('Darb', 'Hubbert');
+insert into people (first_name, last_name) values ('Rafe', 'Killelea');
+insert into people (first_name, last_name) values ('Jim', 'Hibling');
