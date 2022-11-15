8372674504c95a5975e8f6ba642b9e63d60937bf
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-14 21:11:56 -0800

    add test for location

--- __tests__/locations.test.js
+++ __tests__/locations.test.js
@@ -8,6 +8,18 @@ describe('Test Locations Routes', () => {
     return setup(pool);
   });
   //tests
+  it('GET /locations should return a list of city, country, and id', async () => {
+    const response = await request(app).get('/locations');
+    const expectedResponse = [
+      { id: 1, country: 'Oslo', city: 'Norway' },
+      { id: 2, country: 'Göteborg', city: 'Sweden' },
+      { id: 3, country: 'Petrovskaya', city: 'Russia' },
+      { id: 4, country: 'Jingling', city: 'China' },
+      { id: 5, country: 'Xiaozhang', city: 'China' },
+    ];
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expectedResponse);
+  });
 });
 
 afterAll(() => {
