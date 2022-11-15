42ec3b07ba520fc8f13a2f8c734784a206913e9b
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-15 10:27:08 -0800

    add test for animals index route

--- __tests__/animals.test.js
+++ __tests__/animals.test.js
@@ -7,7 +7,51 @@ describe('test', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('test', async () => {});
+
+  it('GET /locations should return a list of city, country, and id', async () => {
+    const response = await request(app).get('/animals');
+    expect(response.status).toBe(200);
+    const expectedResponse = [
+      {
+        id: '1',
+        common_name: 'Common zorro',
+        scientific_name: 'Dusicyon thous',
+      },
+      {
+        id: '2',
+        common_name: 'Lion, south american sea',
+        scientific_name: 'Otaria flavescens',
+      },
+      {
+        id: '3',
+        common_name: 'Collared peccary',
+        scientific_name: 'Tayassu tajacu',
+      },
+      {
+        id: '4',
+        common_name: 'Ground monitor (unidentified)',
+        scientific_name: 'Varanus sp.',
+      },
+      {
+        id: '5',
+        common_name: 'Seal, northern elephant',
+        scientific_name: 'Mirounga angustirostris',
+      },
+      {
+        id: '6',
+        common_name: 'Dove, white-winged',
+        scientific_name: 'Zenaida asiatica',
+      },
+      {
+        id: '7',
+        common_name: 'Grenadier, common',
+        scientific_name: 'Uraeginthus granatina',
+      },
+      { id: '8', common_name: 'Nyala', scientific_name: 'Tragelaphus angasi' },
+    ];
+
+    expect(response.body).toEqual(expectedResponse);
+  });
 });
 
 afterAll(() => {
