3e7e30971d839c96a9a818396e9de6991409a0b4
Erik M Baxstrom <baxstrem@gmail.com>
2022-11-18 15:15:38 -0800

    add test for plants index route

--- __tests__/plants.test.js
+++ __tests__/plants.test.js
@@ -8,8 +8,30 @@ describe('Test Plants Routes', () => {
     return setup(pool);
   });
 
-  it('test', async () => {
-    //test
+  it('GET /plants should return a list of plants', async () => {
+    const response = await request(app).get('/plants');
+    expect(response.status).toBe(200);
+    const expectedResponse = [
+      {
+        common_name: 'Cleveland Cryptantha',
+        scientific_name: 'Cryptantha clevelandii Greene var. clevelandii',
+      },
+      {
+        common_name: 'Fremont Deathcamas',
+        scientific_name: 'Zigadenus fremontii (Torr.) Torr. ex S. Watson',
+      },
+      {
+        common_name: 'Oakwoods Ponysfoot',
+        scientific_name: 'Dichondra recurvata Tharp & M.C. Johnst.',
+      },
+      { common_name: 'Myrinia Moss', scientific_name: 'Myrinia Schimp.' },
+      {
+        common_name: 'Murpheys Century Plant',
+        scientific_name: 'Agave murpheyi F. Gibson',
+      },
+    ];
+
+    expect(response.body).toEqual(expectedResponse);
   });
 });
 
